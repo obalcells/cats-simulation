33ce2add715b40b46c009c77ec40f794fc3822f0
Jonas Binz <jbinz@student.ethz.ch>
2021-05-28 17:00:40 +0200

    Added Sensor stuff and bugfixes

--- rocket_sim/rigid_body_model.m
+++ rocket_sim/rigid_body_model.m
@@ -1,4 +1,4 @@
-function equation = rigid_body_model(y, u, drag_force, parachute_forces, J, J_inv)
+function equation = rigid_body_model(y, u, air_density, C_d, A, parachute_forces, J, J_inv)
 %eq_rigid_solo Propagates the rigid body.
 % u = [force; torque]
 % y = [attitude; position_dq; angular_velocity; linear_velocity] in R16x1
@@ -10,17 +10,20 @@ u_act = [0; u(1); u(2); u(3); 0; u(4); u(5); u(6)];
 % changed_vel = [y(12); y(9:11); y(16); y(13:15)];
 
 q_dot = 0.5*dual_quat_mult(y(1:8), y(9:16));
-drag_B = 0;
 
 gravity_B = dual_quat_mult(dual_quat_mult((y(1:8)),[0; 0; 0; -9.81; 0; 0; 0; 0]), conjugate_dq(y(1:8)));
 thrust = u_act;
-% drag_B = -dual_quat_mult(dual_quat_mult(conjugate_dq(y(1:8)),drag_force), (y(1:8)));
-drag_B = drag_force;
+square_velocity = [sign(y(13))*y(13)^2; sign(y(14))*y(14)^2; sign(y(15))*y(15)^2; sign(y(16))*y(16)^2];
+drag_B = -0.5*air_density*[square_velocity; zeros(4,1)]*C_d*A;
+% drag_B = -0.5*sign(y(13:16))*air_density*[y(13:16); zeros(4,1)].^2*C_d*A;
+% drag_B = dual_quat_mult(dual_quat_mult((y(1:8)),drag_B), conjugate_dq(y(1:8)));
+
 % thrust = dual_quat_mult(dual_quat_mult((y(1:8)),u_act), conjugate_dq(y(1:8)));
 % gravity_B = [0; 0; 0; -9.81; 0; 0; 0; 0];
 parachute_B = dual_quat_mult(dual_quat_mult((y(1:8)),parachute_forces), conjugate_dq(y(1:8)));
+force = thrust + gravity_B + drag_B + parachute_B;
 
-w_dot = J_inv*(thrust + gravity_B + drag_B + parachute_B - dq_cross(y(9:16), J*y(9:16)));
+w_dot = J_inv*(force - dq_cross(y(9:16), J*y(9:16)));
 
 
 equation = [q_dot; w_dot];
--- rocket_sim/rocket_sim.m
+++ rocket_sim/rocket_sim.m
@@ -1,37 +1,48 @@
-% Define Variables %
-total_rocket_mass = 20; % kg
-m_dot = -0.5;                % loss of mass [kg/s]
-sim_time = 100;              % sim time [s]
-diameter_rocket = 0.2;      %m
-burnout_time = 2;           %s
-height_rocket = 1.5;
-main_chute_height = 20;
-p_0 = 1013250;
-Ts = 100;                    % sampling period [Hz]
-air_density = 1.2754; %kg/m^3
-C_d = 0.2; %drag_coefficient;
-A = (diameter_rocket/2)^2*pi;
-gravity = 9.81;
-launch_rail_angle = 88; % degree
+%% Define Variables %%
+
+% Rocket Parameters %
+total_rocket_mass = 2;                          % Rocket Mass loaded[kg]
+m_dot = -0.5;                                   % loss of mass [kg/s]
+diameter_rocket = 0.2;                          % Rocket Diameter [m]
+burnout_time = 0.8;                             % Burnout Time of Motor [s]
+average_thrust_motor = 300;                     % Average Motor Thrust [N]
+height_rocket = 1.5;                            % Rocket Height [m]
+drogue_chute_area = 0.16;                       % Drogue chute Area [m^2]
+main_chute_area = 1.47;                         % Main chute Area [m^2]
+main_chute_height = 150;                        % Height at which the Main should be triggered [m]
+
+% Simulation Parameters %
+sim_time = 1000;                                % sim time [s]
+p_0 = 1013250;                                  % Zero Pressure [Pa]
+Ts = 50;                                       % sampling period [Hz]
+air_density = 1.2754;                           % Air density [kg/m^3]
+C_d = 0.75;                                     % Drag Coefficient [-]
+C_d_chute = 1.75;
+A = (diameter_rocket/2)^2*pi;                   % Rocket Area for Drag [m^2]
+gravity = 9.81;                                 % Gravity [m/s^2]
+launch_rail_angle = 85;                         % Launch rail Angle [°]
+
+%% Prepare Variables %%
 eul = [(launch_rail_angle-90)/180*pi 0 0];
 num_iterations = sim_time * Ts;
-log_acc = zeros(num_iterations, 1);
-log_velocity = zeros(num_iterations, 1);
-log_height = zeros(num_iterations, 1);
+% Logging %
+log_acc = zeros(num_iterations, 3);
+log_lin_vel_B = zeros(num_iterations, 3);
+log_lin_vel_I = zeros(num_iterations, 3);
+log_ang_vel_B = zeros(num_iterations, 3);
+log_pressure = zeros(num_iterations, 1);
 log_pos = zeros(num_iterations, 3);
 log_attitude = zeros(num_iterations, 4);
 log_mass = zeros(num_iterations, 1);
 log_time = zeros(num_iterations, 1);
-acc_sensor_type = 0;
-baro_sensor_type = 1;
+% %
 init_cond = zeros(1,16);
 init_cond(1:4) = eul2quat(eul, 'XYZ');
 thrust = zeros(num_iterations, 1);
-thrust(1:burnout_time*Ts) = 80*ones(burnout_time*Ts, 1);
+thrust(1:burnout_time*Ts) = average_thrust_motor*ones(burnout_time*Ts, 1);
 u = zeros(num_iterations, 6);
 u(:, 3) = thrust;
 I = diag([total_rocket_mass*(1/4*(diameter_rocket/2)^2+1/3*height_rocket^2), total_rocket_mass*(1/4*(diameter_rocket/2)^2+1/3*height_rocket^2), 1/2*total_rocket_mass*(diameter_rocket/2)^2]);
-% I = diag([1/12*total_rocket_mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/12*total_rocket_mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/12*total_rocket_mass*(3*(diameter_rocket/2)^2+height_rocket^2)]);
 J = [zeros(4), [1, zeros(1,3); zeros(3,1), total_rocket_mass*eye(3)];...
     [1, zeros(1,3); zeros(3,1), I], zeros(4)];
 J_inv = eye(8)/J;
@@ -44,67 +55,114 @@ activate_main = 0;
 apogee_time = 0;
 for t = 1:num_iterations
     
-    %     if t > burnout_time*Ts
-    %         mass = mass - m_dot/Ts;
-    %     end
-    %     I = diag([1/12*mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/12*mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/2*mass*(diameter_rocket/2)^2]);
-    %     J = [zeros(4), [1, zeros(1,3); zeros(3,1), mass*eye(3)];...
-    %                     [1, zeros(1,3); zeros(3,1), I], zeros(4)];
-    %     J_inv = eye(8)/J;
+    if t > burnout_time*Ts
+        mass = mass - m_dot/Ts;
+    end
+    I = diag([1/12*mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/12*mass*(3*(diameter_rocket/2)^2+height_rocket^2), 1/2*mass*(diameter_rocket/2)^2]);
+    J = [zeros(4), [1, zeros(1,3); zeros(3,1), mass*eye(3)];...
+        [1, zeros(1,3); zeros(3,1), I], zeros(4)];
+    J_inv = eye(8)/J;
     
     % get velocity %
-    %     [acceleration, velocity, height, mass] = rocket_eq(velocity, height, mass, c, m_dot, crosssection_rocket, C_d, H, p_0, gravity, Ts);
     if t > 10
         if((log_pos(t-2, 3) > log_pos(t-1, 3)) && (activate_parachute == 0))
             activate_parachute = 1;
             apogee_time = t;
-              A = A*5;
+            A = drogue_chute_area;
+            C_d = C_d_chute;
         end
         
         if(log_pos(t-1, 3) < main_chute_height && activate_main == 0 && activate_parachute)
             activate_main = 1;
             main_time = t;
-            A = A*50;
+            A = main_chute_area;
         end
     end
     
     if activate_parachute
-         parachute_forces = [0, 0, 6, 0, 0, 0, 0, 0];
+        parachute_forces = [0, 0, 6, 0, 0, 0, 0, 0];
     end
     if activate_main
-         parachute_forces = parachute_forces + [0, 0, 0, 0, 0, rand(), rand(), rand()];
+        parachute_forces = parachute_forces + [0, 0, 0, 0, 0, rand(), rand(), rand()];
     end
-
-    
-    
-    drag_force = 0.5*air_density*[init_cond(13:16), zeros(1,4)].^2*C_d*A;
-    [solver_time, y_out] = ode45(@(solver_time,y_out) rigid_body_model(y_out, u(t,:), drag_force', parachute_forces', J, J_inv), tspan(t):0.01:tspan(t+1), init_cond);
+        
+    [solver_time, y_out] = ode45(@(solver_time,y_out) rigid_body_model(y_out, u(t,:), air_density, C_d, A, parachute_forces', J, J_inv), tspan(t):0.01:tspan(t+1), init_cond);
     init_cond = y_out(end,:);
+    
+    % Compute Forces %
+    gravity_B = dual_quat_mult(dual_quat_mult((init_cond(1:8)),[0; 0; 0; -9.81; 0; 0; 0; 0]), conjugate_dq(init_cond(1:8)));
+    u_act = [0; u(t, 1); u(t, 2); u(t, 3); 0; u(t, 4); u(t, 5); u(t, 6)];
+    thrust_B = u_act;
+    square_velocity = [sign(init_cond(13))*init_cond(13)^2; sign(init_cond(14))*init_cond(14)^2; sign(init_cond(15))*init_cond(15)^2; sign(init_cond(16))*init_cond(16)^2];
+    drag_B = -0.5*air_density*[square_velocity; zeros(4,1)]*C_d*A;
+    parachute_B = dual_quat_mult(dual_quat_mult((init_cond(1:8)),parachute_forces'), conjugate_dq(init_cond(1:8)));
+    force = gravity_B + thrust_B + drag_B' + parachute_B;
+    
     % Logging %
-    % Position %
     pos = 2*quat_mult(init_cond(5:8), conjugate_q(init_cond(1:4)));
-    %     log_acc(t) = acceleration;
-    %     log_velocity(t) = velocity;
-    log_pos(t,:) = pos(2:4);
-    log_attitude(t, :) = init_cond(1:4);
-    %     log_mass(t) = mass;
+    
+    log_pos(t,:) = pos(2:4);                        % [m]
+    log_attitude(t, :) = init_cond(1:4);            % [quat]
+    log_lin_vel_B(t, :) = init_cond(14:16);           % [m/s]
+    log_ang_vel_B(t, :) = init_cond(10:12)/pi*180;    % [°/s]
+    holder = quat_mult(quat_mult(init_cond(1:4)', [0, init_cond(14:16)]'), conjugate_q(init_cond(1:4)'));
+    log_lin_vel_I(t, :) = holder(2:4);
+    log_acc(t, :) = 1/total_rocket_mass*force(2:4);  % [m/s^2]
+    log_mass(t) = mass;                             % [kg]
     %     log_time(t) = t/Ts;
     if pos(4) < 0
         num_iterations = t;
         break;
     end
 end
+%% Save in datastructure %
+% Pressure Data %
+pressure_sensor = p_0*exp(-gravity*0.0289644*(log_pos(1:num_iterations, 3))/(8.3144598*273.15));
+press_data = [pressure_sensor, pressure_sensor, pressure_sensor];
+
+% Acceleration Data %
+acc_data = [log_acc(1:num_iterations, :), log_acc(1:num_iterations, :), log_acc(1:num_iterations, :)];
+
+% Gyro Data %
+gyro_data = [log_ang_vel_B(1:num_iterations, :), log_ang_vel_B(1:num_iterations, :), log_ang_vel_B(1:num_iterations, :)];
+
+% Add Noise %
+% parameters %
+std_pressure = 100;                     %[Pa]
+bias_pressure = 10*rand(1,3);           %[Pa]
+std_gyro = 5;                           %[°/s]
+bias_gyro = 0.1*rand(3,3);              %[°/s]
+std_acc = 0.01;                         %[m/s^2]
+bias_acc = 0.1*rand(3,3);               %[m/s^2]
+
+% Adding it %
+pressure_noise = [normrnd(bias_pressure(1), std_pressure, num_iterations, 1), normrnd(bias_pressure(1), std_pressure, num_iterations, 1), normrnd(bias_pressure(1), std_pressure, num_iterations, 1)];
+gyro_noise = [normrnd(bias_gyro(1,1), std_gyro, num_iterations, 1), normrnd(bias_gyro(1,2), std_gyro, num_iterations, 1), ...
+    normrnd(bias_gyro(1,3), std_gyro, num_iterations, 1), normrnd(bias_gyro(2,1), std_gyro, num_iterations, 1), ...
+    normrnd(bias_gyro(2,2), std_gyro, num_iterations, 1), normrnd(bias_gyro(2,3), std_gyro, num_iterations, 1), ...
+    normrnd(bias_gyro(3,1), std_gyro, num_iterations, 1), normrnd(bias_gyro(3,2), std_gyro, num_iterations, 1), ...
+    normrnd(bias_gyro(3,3), std_gyro, num_iterations, 1)];
+acc_noise = [normrnd(bias_acc(1,1), std_acc, num_iterations, 1), normrnd(bias_acc(1,2), std_acc, num_iterations, 1), ...
+    normrnd(bias_acc(1,3), std_acc, num_iterations, 1), normrnd(bias_acc(2,1), std_acc, num_iterations, 1), ...
+    normrnd(bias_acc(2,2), std_acc, num_iterations, 1), normrnd(bias_acc(2,3), std_acc, num_iterations, 1), ...
+    normrnd(bias_acc(3,1), std_acc, num_iterations, 1), normrnd(bias_acc(3,2), std_acc, num_iterations, 1), ...
+    normrnd(bias_acc(3,3), std_acc, num_iterations, 1)];
+pressure_noisy = press_data + pressure_noise;
+gyro_noisy = gyro_data + gyro_noise;
+acc_noisy = acc_data + acc_noise;
+
 %%
 figure(1)
-plot3(log_pos(:, 1), log_pos(:, 2), log_pos(:, 3))
+%plot3(log_pos(:, 1), log_pos(:, 2), log_pos(:, 3))
+
 grid on;
-% time = 1/Ts:1/Ts:sim_time;
-% subplot(2,2,1)
-% plot(time, log_height)
+time = 1/Ts:1/Ts:sim_time;
+subplot(2,1,1)
+plot(time, log_pos(:, 3))
 % title("Height")
-% subplot(2,2,2)
-% plot(time, log_velocity)
-% title("Velocity")
+subplot(2,1,2)
+plot(time, log_lin_vel_I(:, 3))
+title("Velocity")
 % subplot(2,2,3)
 % plot(time, log_acc)
 % title("Acceleration")
